3843a8725c42693b35e34c12d398898749073a4d
Airtonmartins <airtonmartinsneris@hotmail.com>
2020-04-10 16:41:02 -0300

    [feature/develop]  Adds Swagger
    * Using the drf_yasg dependency to create the Swagger API documentation

--- work_at_olist/library/views.py
+++ work_at_olist/library/views.py
@@ -3,6 +3,9 @@ from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
@@ -13,6 +16,11 @@ class AuthorList(generics.ListAPIView):
     """
     serializer_class = AuthorSerializer
 
+    @swagger_auto_schema(operation_description="Retrieve list of Authors",
+        manual_parameters=[
+            openapi.Parameter(name='name', in_=openapi.IN_PATH,
+                type=openapi.TYPE_INTEGER)], 
+        responses={200: AuthorSerializer(many=True)})
     def get_queryset(self):
         authors = Author.objects.all()
         name = self.request.query_params.get('name', None)
@@ -26,11 +34,16 @@ class BookList(APIView):
     Create a new Book.
     """
 
+    @swagger_auto_schema(operation_description="Retrieve list of Books", 
+        responses={200: BookSerializer(many=True)})
     def get(self, request, format=None):
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
 
+    @swagger_auto_schema(operation_description="Create a new Book", 
+        request_body=BookSerializer(),
+        responses={200: BookSerializer()})
     def post(self, request, format=None):
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
@@ -50,11 +63,16 @@ class BookDetail(APIView):
         except Book.DoesNotExist:
             raise Http404
     
+    @swagger_auto_schema(operation_description="Retrieve a Book instance", 
+        responses={200: BookSerializer()})
     def get(self, request, pk, format=None):
         book = self.get_object(pk)
         serializer = BookSerializer(book)
         return Response(serializer.data)
 
+    @swagger_auto_schema(operation_description="Update a Book instance", 
+        request_body=BookSerializer(),
+        responses={200: BookSerializer()})
     def put(self, request, pk, format=None):
         book = self.get_object(pk)
         serializer = BookSerializer(book, data=request.data)
@@ -63,6 +81,7 @@ class BookDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @swagger_auto_schema(operation_description="Delete a Book instance")
     def delete(self, request, pk, format=None):
         book = self.get_object(pk)
         book.delete()
